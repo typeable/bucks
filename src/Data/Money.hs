37b155c69a8b9fa463fddb0c117d35827c098d58
Nick <nick@typeable.io>
2022-11-28 13:37:43 +0000

    Fix package flags.

--- src/Data/Money.hs
+++ src/Data/Money.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE CPP #-}
 module Data.Money (module Export) where
 
+import Data.Money.Currencies as Export
 import Data.Money.Internal as Export
 
-#ifdef flag_AESON
+#ifdef USE_AESON
 import Data.Money.JSON as Export ()
 #endif
-#ifdef flag_REL8
+#ifdef USE_REL8
 import Data.Money.Rel8 as Export ()
 #endif
