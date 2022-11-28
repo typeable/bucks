871cbf3835837d161fca3aaef6d6cc41553cee09
Nick <nick@typeable.io>
2022-11-28 11:47:12 +0000

    Rel8 support.

--- src/Data/Money.hs
+++ src/Data/Money.hs
@@ -6,3 +6,6 @@ import Data.Money.Internal as Export
 #ifdef flag_AESON
 import Data.Money.JSON as Export ()
 #endif
+#ifdef flag_REL8
+import Data.Money.Rel8 as Export ()
+#endif
---
+++ src/Data/Money/Rel8.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+module Data.Money.Rel8 () where
+
+import Data.Money.Internal
+
+import Data.Scientific
+import Rel8
+
+instance DBType (Money curr) where
+  typeInformation =
+    let sci = typeInformation @Scientific
+    in TypeInformation { encode = encode sci . realToFrac . getAmount
+                       , decode = fmap (Money . realToFrac) (decode sci)
+                       , typeName = "money"
+                       }
+
